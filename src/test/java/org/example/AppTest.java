ee0929cbc47729d3d5c0972aa4dba1ab0cea40e6
Иван Дорофеев <dfyz.20032020@gmail.com>
2023-12-23 11:10:52 +0300

    Laboratory work 5

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,6 +4,10 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 /**
  * Unit test for simple App.
  */
@@ -34,5 +38,25 @@ public class AppTest
     public void testApp()
     {
         assertTrue( true );
+    }
+
+    public void testInject() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        Bmd bmd;
+
+        try {
+            bmd = (new Injector()).inject(new Bmd());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        }
+
+        bmd.land();
+        assertEquals("Driving LADA\r\nFlying Airbus\r\n", output.toString());
     }
 }
